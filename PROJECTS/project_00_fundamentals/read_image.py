58e4099bc46088b30bc2625b32936c232e08c87a
Santiago <santiago.garcia1999@hotmail.com>
2020-08-14 11:24:14 -0500

    Changes in comments and path strcture to acces imgs

--- PROJECTS/project_00_fundamentals/read_image.py
+++ PROJECTS/project_00_fundamentals/read_image.py
@@ -1,24 +1,23 @@
-#READ AN IMAGE WITH 
-#Santiago Garcia Arango, June 2020
+# READ AN IMAGE WITH 
+# Santiago Garcia Arango, June 2020
 
 import cv2
 import os
 
-#Get the path for the folder that contains the images (two directories up, and folder "imgs")
-img_folder_path =  os.path.dirname( os.path.dirname( __file__ ) ) + "\\imgs\\" 
-print( img_folder_path )
+# Get the path for the folder that contains the images
+upper_dir = os.path.dirname(os.path.dirname(__file__))  # Two dirs up
+img_folder_path =  os.path.abspath(os.path.join(upper_dir, "imgs"))
+print("\nPATH TO IMAGES: ", img_folder_path)
 
 
-print("This is how we read an image!!!")
-img = cv2.imread(img_folder_path + "astronaut.jpg")
+print("\nTHIS IS HOW WE READ AN IMAGE :)")
+img = cv2.imread(os.path.join(img_folder_path, "astronaut.jpg"))
 img = cv2.resize(img, (int(300),int(500)) )
 
 cv2.imshow('Original' , img)
 
-#Allow image to show up and will be there until a key is pressed
+# Allow image to show up and will be there until a key is pressed
 cv2.waitKey(0)
 
-#Final operation in process that collapses all images when is executed (always at the end)
-cv2.destroyAllWindows()
-
-
+# Final operation in process that collapses all images when is executed 
+cv2.destroyAllWindows()  # (always at the end)
