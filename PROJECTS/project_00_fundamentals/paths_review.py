cad9250dc3682ede8e35fde7cb1231bac6f5b593
Santiago <santiago.garcia1999@hotmail.com>
2020-08-14 23:36:23 -0500

    Added new methods to correctly access upper folders and commented code

--- PROJECTS/project_00_fundamentals/paths_review.py
+++ PROJECTS/project_00_fundamentals/paths_review.py
@@ -1,8 +1,21 @@
-#REVIEW OF PATHS IN PYTHON TO ACCESS OTHER DIRECTORIES
-#Santiago Garcia Arango, June 2020
+# REVIEW OF PATHS IN PYTHON TO ACCESS OTHER DIRECTORIES
+# Santiago Garcia Arango, August 2020
 
 import os
-print( os.getcwd() ) #Current work directory (can change based on the directory from where this is run)
-print( __file__ ) #Current path for this specific python file (always the same)
-print( os.path.dirname( __file__) ) #Access the "parent directory" of the current python file executed
-print( os.path.dirname( os.path.dirname( __file__ ) ) ) #Access upper folder two levels
+
+# Current Work Directory (Can change based on the directory where this is run)
+print("\nOS.GETCWD():  ", os.getcwd())
+
+# Current path for this specific python file (always the same)
+print("\n__FILE__: ", __file__)
+
+# Access the "parent directory" of the current python file executed
+print("\nOS.PATH.DIRNAME(__FILE__): ", os.path.dirname(__file__) )
+
+# Access upper folder two levels
+upper_folder = os.path.dirname(os.path.dirname(__file__))
+print("\nOS.PATH.DIRNAME(OS.PATH.DIRNAME(__FILE__)): ", upper_folder)
+
+# Join path of folder with an specific sub-folder name (in this case "imgs")
+new_path = os.path.abspath(os.path.join(upper_folder, "imgs"))
+print("\nOS.PATH.ABS(OS.PATH.JOIN(upper_folder, \"imgs\")): ", new_path)
