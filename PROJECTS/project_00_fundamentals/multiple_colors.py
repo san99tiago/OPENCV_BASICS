14af22b47a3189f639ccb09676f8562334f5d12a
Santiago <santiago.garcia1999@hotmail.com>
2020-08-15 01:58:28 -0500

    Added upper folder package to save images easier and use later

--- PROJECTS/project_00_fundamentals/multiple_colors.py
+++ PROJECTS/project_00_fundamentals/multiple_colors.py
@@ -5,11 +5,13 @@ import numpy as np
 import cv2 as cv
 import os
 
+# This way we can access upper package to save images in output folder
+os.sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from SaveImg import save_img
+
 # Get the path for the folder that contains the input and output images
-upper_dir = os.path.dirname(os.path.dirname(__file__))  # Two dirs up
+upper_dir = os.path.dirname(__file__)  # Upper dir
 img_folder_path = os.path.abspath(os.path.join(upper_dir, "imgs"))
-output_folder_path = os.path.abspath(os.path.join(upper_dir, "output"))
-print("\nPATH TO IMAGES: ", img_folder_path)
 
 # Get specific path to the images we will use inside the "imgs" folder
 path_delfin = os.path.join(img_folder_path, "delfin.jpg")
@@ -35,6 +37,14 @@ cv.imshow("My color image", imgColor)
 cv.imshow("My blue copy", blue_copy)
 cv.imshow("My red copy", red_copy)
 cv.imshow("My green copy", green_copy)
+
+# Save obtained images
+try:
+    save_img.SaveResult(blue_copy, "blue.jpg", __file__)
+    save_img.SaveResult(red_copy, "red.jpg", __file__)
+    save_img.SaveResult(green_copy,  "green.jpg", __file__)
+except:
+    pass
 
 cv.waitKey(0)  # Show image until a key is pressed
 cv.destroyAllWindows()  # (always at the end)
