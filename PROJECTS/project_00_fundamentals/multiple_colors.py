3fab33ec3df46979ebc86bcb4b89ac668a34bbb2
Santiago <santiago.garcia1999@hotmail.com>
2020-08-14 23:25:01 -0500

    Changed second try of solving problem, kept simple solution

--- PROJECTS/project_00_fundamentals/multiple_colors.py
+++ PROJECTS/project_00_fundamentals/multiple_colors.py
@@ -5,9 +5,10 @@ import numpy as np
 import cv2 as cv
 import os
 
-# Get the path for the folder that contains the images
+# Get the path for the folder that contains the input and output images
 upper_dir = os.path.dirname(os.path.dirname(__file__))  # Two dirs up
-img_folder_path =  os.path.abspath(os.path.join(upper_dir, "imgs"))
+img_folder_path = os.path.abspath(os.path.join(upper_dir, "imgs"))
+output_folder_path = os.path.abspath(os.path.join(upper_dir, "output"))
 print("\nPATH TO IMAGES: ", img_folder_path)
 
 # Get specific path to the images we will use inside the "imgs" folder
@@ -16,28 +17,19 @@ path_delfin = os.path.join(img_folder_path, "delfin.jpg")
 # Loading our image with its path and parameter to change format
 imgColor = cv.imread(path_delfin, 1)
 
-h,w = imgColor.shape[:2]  # Get the shape of the image (height, width, c)
+h, w = imgColor.shape[:2]  # Get the shape of the image (height, width, c)
 
 # Now we play to generate 3 new images with Blue, Green, Red backgrounds
-blue_copy = np.zeros((h,w,3), np.uint8)
-red_copy = np.zeros((h,w,3), np.uint8)
-green_copy = np.zeros((h,w,3), np.uint8)
+blue_copy = np.zeros((h, w, 3), np.uint8)
+red_copy = np.zeros((h, w, 3), np.uint8)
+green_copy = np.zeros((h, w, 3), np.uint8)
 
 
 for i in range(0, h):
     for j in range(0, w):
-        # blue_copy[i,j] = [imgColor[i,j][0], 0, 0]  # Only save Blue data
-        green_copy[i,j] = [0, imgColor[i,j][1], 0]  # Only save Green data
-        red_copy[i,j] = [0, 0, imgColor[i,j][2]]  # Only save Red data
-
-# blue_copy[:] = 
-
-print(imgColor)
-print(imgColor[0, 0])
-
-# print(imgColor[10, 30])
-# print(blue_copy[10, 30])
-# print(red_copy[10, 30])
+        blue_copy[i, j] = [imgColor[i, j][0], 0, 0]  # Only save Blue data
+        green_copy[i, j] = [0, imgColor[i, j][1], 0]  # Only save Green data
+        red_copy[i, j] = [0, 0, imgColor[i, j][2]]  # Only save Red data
 
 cv.imshow("My color image", imgColor)
 cv.imshow("My blue copy", blue_copy)
