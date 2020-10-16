0f43eff322cf94eeae6347f8ce8ad8911d5bc18c
Santiago <santiago.garcia1999@hotmail.com>
2020-10-16 01:26:48 -0500

    Improved characteristics extracted for the image recognition process

--- PROJECTS/project_06_fundamentals/extract_characteristics.py
+++ PROJECTS/project_06_fundamentals/extract_characteristics.py
@@ -21,6 +21,65 @@ def get_img_path(img_folder_path, img_name):
     return img_path
 
 
+def extract_characteristics(path):
+    img = cv.imread(path, 0)
+    imgColor = cv.imread(path, 1)
+    img = cv.resize(img, (30, 60))
+    imgColor = cv.resize(imgColor, (30, 60))
+    ret, imgBin = cv.threshold(img, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
+    cnt, hie = cv.findContours(imgBin, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+
+    check_correct_contour = 0
+    for cont in cnt:
+        x, y, w, h = cv.boundingRect(cont)
+        
+        if (cv.contourArea(cont) > 200):
+            check_correct_contour = check_correct_contour + 1
+            # Draw rectangle on top of img
+            cv.rectangle(imgColor, (x,y), (x+w, y+h), (255, 0, 0), 2)
+
+            # Show current img and contour for a few seconds
+            cv.imshow("imgColor", imgColor)
+            cv.waitKey(1)
+
+            # Extract characteristics
+            area = w*h
+            areaf = cv.contourArea(cont)
+            aspect_relation = w/h
+            perim = cv.arcLength(cont, 1)
+            M = cv.moments(cont)
+            Hu = cv.HuMoments(M)  # Get Hu moments
+
+            # Get centroids
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+
+            pixeles_h_2 = 0
+            for h_i in range(h//2):
+                for w_i in range(w):
+                    if imgBin[h_i, w_i] == 255:
+                        pixeles_h_2 = pixeles_h_2 + 1
+
+            pixeles_w_2 = 0
+            for h_i in range(h):
+                for w_i in range(w//2):
+                    if img[h_i, w_i] == 255:
+                        pixeles_w_2 = pixeles_w_2 + 1
+
+
+            # Create vector with this important information
+            vector_characteristics = np.array([area, areaf,
+                        aspect_relation, perim, Hu[0][0],
+                        Hu[1][0], Hu[2][0], Hu[3][0], Hu[4][0],
+                        cX, cY,
+                        pixeles_h_2, pixeles_w_2], dtype= np.float)
+
+    # Only return vector when the contours got the correct characteristics
+    if check_correct_contour == 0:
+        return None
+    else:
+        return vector_characteristics
+
 def main():
     # Create vector to go through each "object" to classify
     vector_folders = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -40,66 +99,22 @@ def main():
 
     # Go to each img in every folder
     for j in range(0, len(vector_folders)):
-        current_path = num_path + "/" + vector_folders[j]
+        current_path = os.path.join(num_path, vector_folders[j])
         for path in glob.glob(current_path + "/*.png"):
-            img = cv.imread(path, 0)
-            imgColor = cv.imread(path, 1)
-            img = cv.resize(img, (30, 60))
-            imgColor = cv.resize(imgColor, (30, 60))
-            ret, imgBin = cv.threshold(img, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
-            cnt, hie = cv.findContours(imgBin, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
 
-            for cont in cnt:
-                x, y, w, h = cv.boundingRect(cont)
-                
-                if (cv.contourArea(cont) > 200):
-                    
-                    # Draw rectangle on top of img
-                    cv.rectangle(imgColor, (x,y), (x+w, y+h), (255, 0, 0), 2)
+            # Main function to extract all characteristics
+            vector_characteristics = extract_characteristics(path)
 
-                    # Show current img and contour for a few seconds
-                    cv.imshow("imgColor", imgColor)
-                    cv.waitKey(1)
+            # Sometimes the characteristics can't be achieved... avoid error
+            if vector_characteristics is not None:
+                # Write characteristics in Excel document
+                for charac in (vector_characteristics):
+                    worksheet.write(row, col, vector_folders[j])
+                    worksheet.write(row, i, charac)
+                    i = i + 1
+                i = 1
+                row = row + 1
 
-                    # Extract characteristics
-                    area = w*h
-                    areaf = cv.contourArea(cont)
-                    aspect_relation = w/h
-                    perim = cv.arcLength(cont, 1)
-                    M = cv.moments(cont)
-                    Hu = cv.HuMoments(M)  # Get Hu moments
-
-                    # Get centroids
-                    cX = int(M["m10"] / M["m00"])
-                    cY = int(M["m01"] / M["m00"])
-
-                    pixeles_h_2 = 0
-                    for h_i in range(h//2):
-                        for w_i in range(w):
-                            if imgBin[h_i, w_i] == 255:
-                                pixeles_h_2 = pixeles_h_2 + 1
-
-                    pixeles_w_2 = 0
-                    for h_i in range(h):
-                        for w_i in range(w//2):
-                            if img[h_i, w_i] == 255:
-                                pixeles_w_2 = pixeles_w_2 + 1
-
-
-                    # Create vector with this important information
-                    vector_characteristics = np.array([area, areaf,
-                                aspect_relation, perim, Hu[0][0],
-                                Hu[1][0], Hu[2][0], Hu[3][0], Hu[4][0],
-                                cX, cY,
-                                pixeles_h_2, pixeles_w_2], dtype= np.float)
-
-                    # Write characteristics in Excel document
-                    for charac in (vector_characteristics):
-                        worksheet.write(row, col, vector_folders[j])
-                        worksheet.write(row, i, charac)
-                        i = i + 1
-                    i = 1
-                    row = row + 1
     workbook.close()
 
 
