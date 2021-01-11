e2cb1155c00b3e6b244e5642b63d0a09d87c8933
Santiago <santiago.garcia1999@hotmail.com>
2021-01-11 02:58:15 -0500

    Updated face_detection to have image as input (instead of img_path)

--- PROJECTS/project_08_face_detection/face_detect.py
+++ PROJECTS/project_08_face_detection/face_detect.py
@@ -9,14 +9,11 @@ import os
 CURRENT_FOLDER = os.path.abspath(os.path.dirname(__file__))
 print(CURRENT_FOLDER)
 
-def main(image_relative_path):
+def main(image):
     # Create the haar cascade
     casc_path = os.path.join(CURRENT_FOLDER, "haarcascade_frontalface_default.xml")  # From OpenCV Github
     faceCascade = cv.CascadeClassifier(casc_path)
 
-    # Read image
-    image_relative_path = os.path.join(CURRENT_FOLDER, image_relative_path)
-    image = cv.imread(image_relative_path)
     if image is None:
         print("ERROR: Image did not load correctly.")
         return False
@@ -29,7 +26,7 @@ def main(image_relative_path):
         scaleFactor=1.15,
         minNeighbors=5,
         minSize=(30, 30),
-        flags = cv.CASCADE_SCALE_IMAGE
+        flags=cv.CASCADE_SCALE_IMAGE
     )
 
     print(faces)
@@ -37,29 +34,32 @@ def main(image_relative_path):
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        cv.rectangle(image, (x, y), (x+w, y+h), (255, 255, 55), 3)
+        cv.rectangle(image, (x, y), (x + w, y + h), (255, 255, 55), 3)
 
     cv.imshow("FACES DETECTED", image)
-    cv.waitKey(0)
 
 
 if __name__ == "__main__":
     # TEST 1
-    image_relative_path = os.path.join("test_imgs", "picture0.jpg")
-    print(image_relative_path)
-    main(image_relative_path)
+    image_relative_path = os.path.join(CURRENT_FOLDER, "test_imgs", "picture0.jpg")
+    image = cv.imread(image_relative_path)
+    main(image)
+    cv.waitKey(0)
 
     # TEST 2
-    image_relative_path = os.path.join("test_imgs", "picture1.PNG")
-    print(image_relative_path)
-    main(image_relative_path)
+    image_relative_path = os.path.join(CURRENT_FOLDER, "test_imgs", "picture1.png")
+    image = cv.imread(image_relative_path)
+    main(image)
+    cv.waitKey(0)
 
     # TEST 3
-    image_relative_path = os.path.join("test_imgs", "picture2.PNG")
-    print(image_relative_path)
-    main(image_relative_path)
+    image_relative_path = os.path.join(CURRENT_FOLDER, "test_imgs", "picture2.png")
+    image = cv.imread(image_relative_path)
+    main(image)
+    cv.waitKey(0)
 
     # TEST 4
-    image_relative_path = os.path.join("test_imgs", "picture3.PNG")
-    print(image_relative_path)
-    main(image_relative_path)
+    image_relative_path = os.path.join(CURRENT_FOLDER, "test_imgs", "picture3.png")
+    image = cv.imread(image_relative_path)
+    main(image)
+    cv.waitKey(0)
